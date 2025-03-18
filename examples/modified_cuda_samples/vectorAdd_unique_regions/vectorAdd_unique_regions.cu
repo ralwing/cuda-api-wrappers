68fda805cb10fdf917d58b435997ea976b465afd
Grzegorz Głowacki <gglowacki@autonomous-systems.pl>
2025-03-18 13:45:52 +0100

    param tests

--- examples/modified_cuda_samples/vectorAdd_unique_regions/vectorAdd_unique_regions.cu
+++ examples/modified_cuda_samples/vectorAdd_unique_regions/vectorAdd_unique_regions.cu
@@ -28,32 +28,40 @@ template <> struct is_gpu_compatible<DataType> : ::std::true_type {};
 } // namespace cuda
 
 __global__ void vectorAdd(const DataType *A, const DataType *B,
-                          DataType *result, int numElements) {
+                          const DataType param, DataType *result,
+                          int numElements) {
   int i = blockDim.x * blockIdx.x + threadIdx.x;
   if (i < numElements) {
-    result[i] = A[i] + B[i];
+    result[i] = A[i] + B[i] + param;
   }
 }
 
 __global__ void vectorAddValue(const DataType A, const DataType B,
-                               DataType *result) {
+                               const DataType param, DataType *result) {
   int i = blockDim.x * blockIdx.x + threadIdx.x;
   if (i < 1) {
-    *result = A + B;
+    *result = A + B + param;
   }
 }
 
-__global__ void vectorAddSpan(const SpanType A, const SpanType B, SpanType C) {
+__global__ void vectorAddSpan(const SpanType A, const SpanType B,
+                              const DataType param, SpanType C) {
   int i = blockDim.x * blockIdx.x + threadIdx.x;
   if (i < C.size()) {
-    C[i] = A[i] + B[i];
+    C[i] = A[i] + B[i] + param;
   }
 }
 
-void print_result(const DataType a, const DataType b, const DataType result) {
-  std::cout << "A: " << a.transpose() << " B: " << b.transpose()
-            << " Result: " << result.transpose() << ". Should be "
-            << (a + b).transpose() << "\n";
+void check_result(const DataType a, const DataType b, const DataType param,
+                  const DataType result) {
+  if (result != (a + b + param)) {
+    std::cout << "Failed: A: " << a.transpose() << " B: " << b.transpose()
+              << " Param: " << param.transpose()
+              << " Result: " << result.transpose() << ". Should be "
+              << (a + b + param).transpose() << "\n";
+  } else {
+    std::cout << "OK: " << result.transpose() << "\n";
+  }
 }
 
 int main() {
@@ -81,6 +89,8 @@ int main() {
         fdistribution(randomness_generator);
     return result;
   };
+
+  DataType param = generator();
 
   std::generate(h_A.begin(), h_A.end(), generator);
   std::generate(h_B.begin(), h_B.end(), generator);
@@ -111,13 +121,14 @@ int main() {
       stream.enqueue.copy(sp_B, h_B);
 
       stream.enqueue.kernel_launch(vectorAdd, launch_config, sp_A.data(),
-                                   sp_B.data(), sp_result.data(), numElements);
+                                   sp_B.data(), param, sp_result.data(),
+                                   numElements);
 
       stream.enqueue.copy(h_result, sp_result);
 
       stream.synchronize();
 
-      print_result(h_A[0], h_B[0], h_result[0]);
+      check_result(h_A[0], h_B[0], param, h_result[0]);
     }
 
     {
@@ -128,12 +139,12 @@ int main() {
       stream.enqueue.copy(sp_B, h_B);
 
       stream.enqueue.kernel_launch(vectorAddSpan, launch_config, SpanType{sp_A},
-                                   SpanType{sp_B}, SpanType{sp_result});
+                                   SpanType{sp_B}, param, SpanType{sp_result});
 
       stream.enqueue.copy(h_result, sp_result);
 
       stream.synchronize();
-      print_result(h_A[0], h_B[0], h_result[0]);
+      check_result(h_A[0], h_B[0], param, h_result[0]);
     }
   }
   {
@@ -147,12 +158,12 @@ int main() {
       DataType *d_result;
       cudaMalloc((void **)&d_result, sizeof(DataType));
 
-      vectorAddValue<<<1, 1>>>(A, B, d_result);
+      vectorAddValue<<<1, 1>>>(A, B, param, d_result);
       cudaDeviceSynchronize();
 
       cudaMemcpy(&result, d_result, sizeof(DataType), cudaMemcpyDeviceToHost);
 
-      print_result(A, B, result);
+      check_result(A, B, param, result);
       cudaFree(d_result);
     }
 
@@ -164,14 +175,11 @@ int main() {
       DataType *d_result;
       cudaMalloc((void **)&d_result, sizeof(DataType));
 
-      auto d_result_region = cuda::memory::make_unique_region(
-          device, numElements * sizeof(DataType));
-      auto sp_result = d_result_region.as_span<DataType>();
-      stream.enqueue.kernel_launch(vectorAddValue, launch_config, A, B,
+      stream.enqueue.kernel_launch(vectorAddValue, launch_config, A, B, param,
                                    d_result);
       stream.enqueue.copy(&result, d_result, sizeof(DataType));
       stream.synchronize();
-      print_result(A, B, result);
+      check_result(A, B, param, result);
 
       cudaFree(d_result);
     }
@@ -184,11 +192,11 @@ int main() {
       auto d_result_region = cuda::memory::make_unique_region(
           device, numElements * sizeof(DataType));
       auto sp_result = d_result_region.as_span<DataType>();
-      stream.enqueue.kernel_launch(vectorAddValue, launch_config, A, B,
+      stream.enqueue.kernel_launch(vectorAddValue, launch_config, A, B, param,
                                    sp_result.data());
       stream.enqueue.copy(&result, sp_result.data(), sizeof(DataType));
       stream.synchronize();
-      print_result(A, B, result);
+      check_result(A, B, param, result);
     }
   }
 }
